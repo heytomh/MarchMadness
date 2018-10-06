8fee052403b43d8348f5720833bdb9b2c4d26eed
tom <generict@gmail.com>
2018-10-06 10:27:37 -0700

    test 2

--- dataCollection.py
+++ dataCollection.py
@@ -1,1 +1,10 @@
 # This will be the starting point for getting data for March Madness
+
+# just some practice
+
+colors = ["red", "blue", "green"]
+
+i = 0
+while i < len(colors):
+    print ("Lets try %s as a test" % colors[i])
+    i += 1
