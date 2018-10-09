715e00e5b5796ea57edb98cd4f8a4e6639930d53
tom <generict@gmail.com>
2018-10-08 18:56:11 -0700

    added first file write

--- dataCollection.py
+++ dataCollection.py
@@ -1,20 +1,23 @@
 import os
+import csv
+
 # This will be the starting point for getting data for March Madness
 
 # create or open the directory
 dataDir = os.path.join('C:\\', 'Data', 'Python Projects', 'MarchMadness', 'MMDataFiles')
 
-print(dataDir)
+# print(dataDir)
+# isItThere = os.path.exists(dataDir)
+# print(isItThere)
+# myFile = open((dataDir + '\\testfile.txt'), 'w')
+# myFile.write('Test Data')
+# myFile.close()
+# myFile = open((dataDir + '\\testfile.txt'), 'a')
+# myFile.write('Test More Data')
+# myFile.close()
 
-isItThere = os.path.exists(dataDir)
+with open((dataDir + '\\testdatafile.csv'), mode='w') as theFile:
+    dataWriter = csv.writer(theFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    dataWriter.writerow(['John', 'Guard', 'UNLV'])
+    dataWriter.writerow(['Barry', 'Guard', 'UCLA'])
 
-print(isItThere)
-
-myFile = open((dataDir + '\\testfile.txt'), 'w')
-
-myFile.write('Test Data')
-myFile.close()
-
-myFile = open((dataDir + '\\testfile.txt'), 'a')
-myFile.write('Test More Data')
-myFile.close()
